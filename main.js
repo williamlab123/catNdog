6cb0321582ad184432fd0e9cddf759fbdb77f732
William <williamprv@outlook.com>
2022-11-16 19:23:02 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,411 @@
+function get()
+{
+   
+   var idade = document.querySelector("#idade").value
+  var  peso = document.querySelector("#peso").value
+
+
+  
+///////////////////////////////////
+
+  if(idade == 1 && peso <= 9)
+  {
+    alert("seu carrapatento tem 22 anos de vivencia")
+  }
+  if(idade == 2 && peso <= 9)
+  {
+    alert("seu carrapatento tem 24 anos de vivencia")
+  }
+  if(idade == 3 && peso <= 9)
+  {
+    alert("seu carrapatento tem 28 anos de vivencia")
+  }
+  if(idade == 4 && peso <= 9)
+  {
+    alert("seu carrapatento tem 32 anos de vivencia")
+  }
+
+  if(idade == 5 && peso <= 9)
+  {
+    alert("seu carrapatento tem 36 anos de vivencia")
+  }
+
+  else if(idade == 6 && peso <= 9)
+  {
+    alert("seu carrapatento tem 40 anos de vivencia")
+  }
+  else if(idade == 7 && peso <= 9)
+  {
+    alert("seu carrapatento tem 44 anos de vivencia")
+  }
+
+  else if(idade == 8 && peso <= 9)
+  {
+    alert("seu carrapatento tem 48 anos de vivencia")
+  }
+
+  else if(idade == 9 && peso <= 9)
+  {
+    alert("seu carrapatento tem 52 anos de vivencia")
+  }
+
+  else if(idade == 10 && peso <= 9)
+  {
+    alert("seu carrapatento tem 56 anos de vivencia")
+  }
+
+  else if(idade == 11 && peso <= 9)
+  {
+    alert("seu carrapatento tem 60 anos de vivencia")
+  }
+
+  else if(idade == 12 && peso <= 9)
+  {
+    alert("seu carrapatento tem 64 anos de vivencia")
+  }
+
+  else if(idade == 13 && peso <= 9)
+  {
+    alert("seu carrapatento tem 68 anos de vivencia")
+  }
+
+  else if(idade == 14 && peso <= 9)
+  {
+    alert("seu carrapatento tem 72 anos de vivencia")
+  }
+
+  else if(idade == 15 && peso <= 9)
+  {
+    alert("seu carrapatento tem 76 anos de vivencia")
+  }
+
+  else if(idade == 16 && peso <= 9)
+  {
+    alert("seu carrapatento tem 80 anos de vivencia")
+  }
+
+  else if(idade == 17 && peso <= 9)
+  {
+    alert("seu carrapatento tem 84 anos de vivencia")
+  }
+
+  else if(idade == 18 && peso <= 9)
+  {
+    alert("seu carrapatento tem 88 anos de vivencia")
+  }
+
+  else if(idade == 19 && peso <= 9)
+  {
+    alert("seu carrapatento tem 92 anos de vivencia")
+  }
+
+  else if(idade == 20 && peso <= 9)
+  {
+    alert("seu carrapatento tem 96 anos de vivencia")
+  }
+
+  
+//////////////////////////////////////////////////
+if(idade == 1 && peso > 9.1 && peso <= 23)
+{
+  alert("seu carrapatento tem 17 anos de vivencia")
+}
+if(idade == 2 && peso > 9.1 && peso <= 23)
+{
+  alert("seu carrapatento tem 22 anos de vivencia")
+}
+if(idade == 3 && peso > 9.1 && peso <= 23)
+{
+  alert("seu carrapatento tem 27 anos de vivencia")
+}
+if(idade == 4 && peso > 9.1 && peso <= 23)
+{
+  alert("seu carrapatento tem 32 anos de vivencia")
+}
+if(idade == 5 && peso > 9.1 && peso <= 23)
+{
+  alert("seu carrapatento tem 37 anos de vivencia")
+}
+
+else if(idade == 6 && peso > 9.1 && peso <= 23)
+{
+  alert("seu carrapatento tem 42 anos de vivencia")
+}
+else if(idade == 7 && peso > 9.1 && peso <= 23)
+{
+  alert("seu carrapatento tem 47 anos de vivencia")
+}
+
+else if(idade == 8 && peso > 9.1 && peso <= 23)
+{
+  alert("seu carrapatento tem 51 anos de vivencia")
+}
+
+else if(idade == 9 && peso > 9.1 && peso <= 23)
+{
+  alert("seu carrapatento tem 56 anos de vivencia")
+}
+
+else if(idade == 10 && peso > 9.1 && peso <= 23)
+{
+  alert("seu carrapatento tem 60 anos de vivencia")
+}
+
+else if(idade == 11 && peso > 9.1 && peso <= 23)
+{
+  alert("seu carrapatento tem 65 anos de vivencia")
+}
+
+else if(idade == 12 && peso > 9.1 && peso <= 23)
+{
+  alert("seu carrapatento tem 69 anos de vivencia")
+}
+
+else if(idade == 13 && peso > 9.1 && peso <= 23)
+{
+  alert("seu carrapatento tem 74 anos de vivencia")
+}
+
+else if(idade == 14 && peso > 9.1 && peso <= 23)
+{
+  alert("seu carrapatento tem 78 anos de vivencia")
+}
+
+else if(idade == 15 && peso > 9.1 && peso <= 23)
+{
+  alert("seu carrapatento tem 83 anos de vivencia")
+}
+
+else if(idade == 16 && peso > 9.1 && peso <= 23)
+{
+  alert("seu carrapatento tem 87 anos de vivencia")
+}
+
+else if(idade == 17 && peso > 9.1 && peso <= 23)
+{
+  alert("seu carrapatento tem 92 anos de vivencia")
+}
+
+else if(idade == 18 && peso > 9.1 && peso <= 23)
+{
+  alert("seu carrapatento tem 96 anos de vivencia")
+}
+
+else if(idade == 19 && peso > 9.1 && peso <= 23)
+{
+  alert("seu carrapatento tem 101 anos de vivencia")
+}
+
+else if(idade == 20 && peso > 9.1 && peso <= 23)  
+{
+  alert("seu carrapatento tem 105 anos de vivencia")
+}
+
+  
+
+ //////////////////////////////////////////////////////////////////////////// 
+ if(idade == 1 && peso > 23.1 && peso <= 40)
+{
+  alert("seu carrapatento tem 19 anos de vivencia")
+}
+if(idade == 2 && peso > 23.1 && peso <= 40)
+{
+  alert("seu carrapatento tem 24 anos de vivencia")
+}
+if(idade == 3 && peso > 23.1 && peso <= 40)
+{
+  alert("seu carrapatento tem 30 anos de vivencia")
+}
+if(idade == 4 && peso > 23.1 && peso <= 40)
+{
+  alert("seu carrapatento tem 35 anos de vivencia")
+}
+
+if(idade == 5 && peso > 23.1 && peso <= 40)
+{
+  alert("seu carrapatento tem 40 anos de vivencia")
+}
+
+else if(idade == 6 && peso > 23.1 && peso <= 40)
+{
+  alert("seu carrapatento tem 45 anos de vivencia")
+}
+else if(idade == 7 && peso > 23.1 && peso <= 40)
+{
+  alert("seu carrapatento tem 50 anos de vivencia")
+}
+
+else if(idade == 8 && peso > 23.1 && peso <= 40)
+{
+  alert("seu carrapatento tem 55 anos de vivencia")
+}
+
+else if(idade == 9 && peso > 23.1 && peso <= 40)
+{
+  alert("seu carrapatento tem 61 anos de vivencia")
+}
+
+else if(idade == 10 && peso > 23.1 && peso <= 40)
+{
+  alert("seu carrapatento tem 66 anos de vivencia")
+}
+
+else if(idade == 11 && peso > 23.1 && peso <= 40)
+{
+  alert("seu carrapatento tem 72 anos de vivencia")
+}
+
+else if(idade == 12 && peso > 23.1 && peso <= 40)
+{
+  alert("seu carrapatento tem 77 anos de vivencia")
+}
+
+else if(idade == 13 && peso > 23.1 && peso <= 40)
+{
+  alert("seu carrapatento tem 82 anos de vivencia")
+}
+
+else if(idade == 14 && peso > 23.1 && peso <= 40)
+{
+  alert("seu carrapatento tem 88 anos de vivencia")
+}
+
+else if(idade == 15 && peso > 23.1 && peso <= 40)
+{
+  alert("seu carrapatento tem 93 anos de vivencia")
+}
+
+else if(idade == 16 && peso > 23.1 && peso <= 40)
+{
+  alert("seu carrapatento tem 99 anos de vivencia")
+}
+
+else if(idade == 17 && peso > 23.1 && peso <= 40)
+{
+  alert("seu carrapatento tem 104 anos de vivencia")
+}
+
+else if(idade == 18 && peso > 23.1 && peso <= 40)
+{
+  alert("seu carrapatento tem 109 anos de vivencia")
+}
+
+else if(idade == 19 && peso > 23.1 && peso <= 40)
+{
+  alert("seu carrapatento tem 115 anos de vivencia")
+}
+
+else if(idade == 20 && peso > 23.1 && peso <= 40) 
+{
+  alert("seu carrapatento tem 120 anos de vivencia")
+}
+
+
+
+////////////////////////////////////////////////////////////
+
+if(idade == 1 && peso > 40.1)
+{
+  alert("seu carrapatento tem 14 anos de vivencia")
+}
+
+if(idade == 2 && peso > 40.1)
+{
+  alert("seu carrapatento tem 21 anos de vivencia")
+}
+
+if(idade == 3 && peso > 40.1)
+{
+  alert("seu carrapatento tem 28 anos de vivencia")
+}
+
+if(idade == 4 && peso > 40.1)
+{
+  alert("seu carrapatento tem 35 anos de vivencia")
+}
+
+if(idade == 5 && peso > 40.1)
+{
+  alert("seu carrapatento tem 42 anos de vivencia")
+}
+
+else if(idade == 6 &&  peso > 40.1)
+{
+  alert("seu carrapatento tem 49 anos de vivencia")
+}
+else if(idade == 7 &&  peso > 40.1)
+{
+  alert("seu carrapatento tem 56 anos de vivencia")
+}
+
+else if(idade == 8 &&  peso > 40.1)
+{
+  alert("seu carrapatento tem 64 anos de vivencia")
+}
+
+else if(idade == 9 &&  peso > 40.1)
+{
+  alert("seu carrapatento tem 71 anos de vivencia")
+}
+
+else if(idade == 10 &&  peso > 40.1)
+{
+  alert("seu carrapatento tem 78 anos de vivencia")
+}
+
+else if(idade == 11 &&  peso > 40.1)
+{
+  alert("seu carrapatento tem 86 anos de vivencia")
+}
+
+else if(idade == 12 &&  peso > 40.1)
+{
+  alert("seu carrapatento tem 93 anos de vivencia")
+}
+
+else if(idade == 13 &&  peso > 40.1)
+{
+  alert("seu carrapatento tem 101 anos de vivencia")
+}
+
+else if(idade == 14 &&  peso > 40.1)
+{
+  alert("seu carrapatento tem 108 anos de vivencia")
+}
+
+else if(idade == 15 &&  peso > 40.1)
+{
+  alert("seu carrapatento tem 115 anos de vivencia")
+}
+
+else if(idade == 16 &&  peso > 40.1)
+{
+  alert("seu carrapatento tem 123 anos de vivencia")
+}
+
+else if(idade == 17 &&  peso > 40.1)
+{
+  alert("seu carrapatento ja devia ter ido de caixa em, da nem pra contar mais")
+}
+
+else if(idade == 18 &&  peso > 40.1)
+{
+    alert("seu carrapatento ja devia ter ido de caixa em, da nem pra contar mais")
+}
+
+else if(idade == 19 &&  peso > 40.1)
+{
+    alert("seu carrapatento ja devia ter ido de caixa em, da nem pra contar mais")
+}
+
+else if(idade == 20 &&  peso > 40.1) 
+{
+    alert("seu carrapatento ja devia ter ido de caixa em, da nem pra contar mais")
+}
+
+else
+{
+  alert("digite uma opçao válida: IDADE VAI ATÉ 20 ANOS")
+}
+
+}
